3fdf6cebec452adb8aca6b8ef65f29f70db2cff5
Ariane Gardou <argardou-ext@airfrance.fr>
2025-01-23 16:45:36 +0100

    script installation

---
+++ 00_installation_script/master.sh
@@ -0,0 +1,36 @@
+echo 
+echo "**** install docker ****"
+echo 
+
+apt-get update
+apt-get install -y docker.io
+
+swapoff -a
+cp /etc/fstab /etc/fstab.bkp
+sed -i.bak '/ swap / s/^\(.*\)$/#/g' /etc/fstab
+
+echo '{"exec-opts": ["native.cgroupdriver=systemd"],"log-driver": "json-file","log-opts": {"max-size": "100m"},"storage-driver": "overlay2"}' > /etc/docker/daemon.json
+systemctl restart docker
+
+sudo apt-get install -y apt-transport-https ca-certificates curl gpg
+curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.30/deb/Release.key | gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.30/deb/ /' | tee /etc/apt/sources.list.d/kubernetes.list
+
+
+echo 
+echo "**** install kubectl, kubeadm and kubelet ****"
+echo 
+
+apt-get update
+apt-get install -y kubeadm kubectl kubelet
+
+
+echo 
+echo "**** init cluster ****"
+echo 
+
+kubeadm init
+
+mkdir -p $HOME/.kube 
+cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+chown  $(id -u):$(id -g)  $HOME/.kube/config
